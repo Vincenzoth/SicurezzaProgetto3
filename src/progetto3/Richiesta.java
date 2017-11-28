e330e65cce259eaf71a3a90831e0482e9ce2562e
michele <rmichele89@gmail.com>
2017-11-28 16:14:23 +0100

    update doc

--- src/progetto3/Richiesta.java
+++ src/progetto3/Richiesta.java
@@ -2,10 +2,16 @@ package progetto3;
 
 import java.io.Serializable;
 
+
 public class Richiesta implements Serializable {
 	private String idUser;
 	private byte[] h; //cif(h)
 	
+	/**
+	 * Costruttore della classe 
+	 * @param idUser Identificativo del mittente 
+	 * @param h contenuto della richiesta in array di byte
+	 */
 	public Richiesta(String idUser, byte[] h) {
 		this.idUser = idUser;
 		this.h = h;
